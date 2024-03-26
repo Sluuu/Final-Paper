48bbd5de14a0f4cadf5922be365b3e55603e0c7e
Sean Liu <yuhsiang.liu@mail.utoronto.ca>
2024-03-25 21:28:28 -0400

    Download raw data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,5 +1,5 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Opendata Toronto
 # Author: Rohan Alexander [...UPDATE THIS...]
 # Date: 11 February 2023 [...UPDATE THIS...]
 # Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
@@ -11,16 +11,20 @@
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
+library(readr)
 
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+#### Download data and save data ####
+download.file(url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/a7ae08f3-c512-4a88-bb3c-ab40eca50c5e/resource/dc4ec2fa-d655-46ca-af32-f216d26e9804/download/Homeless%20deaths%20by%20month.csv", 
+              destfile = "data/raw_data/raw_data_month.csv")
+
+download.file(url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/a7ae08f3-c512-4a88-bb3c-ab40eca50c5e/resource/ceafdcdf-5f41-4ba5-9b69-f4c8e581ab6e/download/Homeless%20deaths%20by%20cause.csv", 
+              destfile = "data/raw_data/raw_data_cause.csv")
+
+download.file(url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/a7ae08f3-c512-4a88-bb3c-ab40eca50c5e/resource/04a75c51-e1ad-4ac3-82bf-e894b16bb9e5/download/Homeless%20deaths%20by%20demographics.csv", 
+              destfile = "data/raw_data/raw_data_demographics.csv")
 
 
 
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
          
