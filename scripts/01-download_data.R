e72043071cc6fc67e2fa6d7ef17fb4599a8d0520
Sean Liu <yuhsiang.liu@mail.utoronto.ca>
2024-03-26 17:16:57 -0400

    Download Raw Data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,11 +1,10 @@
 #### Preamble ####
 # Purpose: Downloads and saves the data from Opendata Toronto
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Sean Liu
+# Date: 2 April 2024 
+# Contact: yuhsiang.liu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+
 
 
 #### Workspace setup ####
@@ -13,8 +12,10 @@ library(opendatatoronto)
 library(tidyverse)
 library(dplyr)
 library(readr)
+library(dataverse)
 
-#### Download data and save data ####
+
+#### Download data and Save data ####
 download.file(url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/a7ae08f3-c512-4a88-bb3c-ab40eca50c5e/resource/dc4ec2fa-d655-46ca-af32-f216d26e9804/download/Homeless%20deaths%20by%20month.csv", 
               destfile = "data/raw_data/raw_data_month.csv")
 
@@ -24,7 +25,8 @@ download.file(url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/a7a
 download.file(url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/a7ae08f3-c512-4a88-bb3c-ab40eca50c5e/resource/04a75c51-e1ad-4ac3-82bf-e894b16bb9e5/download/Homeless%20deaths%20by%20demographics.csv", 
               destfile = "data/raw_data/raw_data_demographics.csv")
 
+download.file(url = "https://databank.worldbank.org/data/download/WDI_CSV.zip", 
+              destfile = "data/raw_data/raw_data_testing.csv")
 
 
 
-         
