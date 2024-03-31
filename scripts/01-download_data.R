786b5c7d80e55633acb8a052c3bb334f69c96606
Sean Liu <yuhsiang.liu@mail.utoronto.ca>
2024-03-31 15:56:56 -0400

    Download and save raw data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,5 +1,5 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from Opendata Toronto
+# Purpose: Downloads and saves the data from The World Bank
 # Author: Sean Liu
 # Date: 2 April 2024 
 # Contact: yuhsiang.liu@mail.utoronto.ca
@@ -8,25 +8,29 @@
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
 library(dplyr)
 library(readr)
 library(dataverse)
+library(WDI)
 
 
-#### Download data and Save data ####
-download.file(url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/a7ae08f3-c512-4a88-bb3c-ab40eca50c5e/resource/dc4ec2fa-d655-46ca-af32-f216d26e9804/download/Homeless%20deaths%20by%20month.csv", 
-              destfile = "data/raw_data/raw_data_month.csv")
-
-download.file(url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/a7ae08f3-c512-4a88-bb3c-ab40eca50c5e/resource/ceafdcdf-5f41-4ba5-9b69-f4c8e581ab6e/download/Homeless%20deaths%20by%20cause.csv", 
-              destfile = "data/raw_data/raw_data_cause.csv")
-
-download.file(url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/a7ae08f3-c512-4a88-bb3c-ab40eca50c5e/resource/04a75c51-e1ad-4ac3-82bf-e894b16bb9e5/download/Homeless%20deaths%20by%20demographics.csv", 
-              destfile = "data/raw_data/raw_data_demographics.csv")
+#### Download data ####
 
 download.file(url = "https://databank.worldbank.org/data/download/WDI_CSV.zip", 
               destfile = "data/raw_data/raw_data_testing.csv")
 
+WDIsearch("gdp growth")
+WDIsearch("inflation")
+WDIsearch("population, total")
+WDIsearch("Unemployment, total")
+
+world_bank_data <-
+  WDI(
+    indicator =
+      c("FP.CPI.TOTL.ZG", "NY.GDP.MKTP.KD.ZG", "SP.POP.TOTL","SL.UEM.TOTL.NE.ZS")
+  )
 
 
+#### Save Data ####
+write_csv(world_bank_data, "data/raw_data/raw_data.csv")
