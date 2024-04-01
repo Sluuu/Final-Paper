7f10dbae15639dde6594c76b6e06c7d28b9f7f43
Sean Liu <yuhsiang.liu@mail.utoronto.ca>
2024-04-01 18:55:37 -0400

    adding a new variable

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,5 +1,5 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
+# Purpose: Cleans the raw data, filtering by year 2008, 2009, and 2020
 # Author: Sean Liu
 # Date: 2 April 2024
 # Contact: yuhsiang.liu@mail.utoronto.ca
@@ -23,14 +23,16 @@ wb_data <-
     population = SP.POP.TOTL,
     unem_rate = SL.UEM.TOTL.NE.ZS,
     lf_par_rate = SL.TLF.CACT.NE.ZS,
-    trade_per = NE.TRD.GNFS.ZS
+    trade_per = NE.TRD.GNFS.ZS, 
+    gdp_per_cap = NY.GDP.PCAP.CD
   ) |>
-  select(country, year, inflation, gdp_growth, population, unem_rate, lf_par_rate, trade_per) |>
+  select(country, year, inflation, gdp_growth, population, unem_rate, lf_par_rate, trade_per, gdp_per_cap) |>
   filter(year %in% c(2020, 2008, 2009))
-
 data_na <- wb_data |> drop_na()
 
 
 
 #### Save data ####
 write_parquet(data_na, "data/analysis_data/data_na.parquet")
+write_csv(data_na, "data/analysis_data/analysis_data.csv")
+
